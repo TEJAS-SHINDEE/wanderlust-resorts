6b66304b566cb330722f17dacdaf2e342c4bad4a
TEJAS-SHINDEE <tejasshindee2024@gmail.com>
2024-07-19 15:28:04 +0530

    fixed issue

--- schema.js
+++ schema.js
@@ -16,6 +16,6 @@ module.exports.listingSchema = joi.object({
 module.exports.reviewSchema = joi.object({
     review : joi.object({
         rating: joi.number().min(1).max(5).required(),
-        Comment : joi.string().required()
+        comment : joi.string().required()
     }).required()
 });
